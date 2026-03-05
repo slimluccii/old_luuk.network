0d2da036f09b391f7b7eec44e9d1f111bbf566b8
Luuk <mail@luuk.network>
2026-03-05 20:07:53 +0100

    Refactor middleware: use context.preferredLocale

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,16 @@
-import { defineMiddleware } from 'astro:middleware';
+import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware((context, next) => {
-  const {
-    cookies,
-    currentLocale,
-    originPathname,
-    preferredLocale,
-    redirect
-  } = context;
+  const { cookies, currentLocale, originPathname, redirect } = context;
 
-  const locale = cookies.get('locale')?.value;
-
-  if (originPathname === '/') {
-    const target = locale ?? preferredLocale;
+  if (originPathname === "/") {
+    const locale = cookies.get("locale")?.value;
+    const target = locale ?? context.preferredLocale;
     if (target) {
       return redirect(`/${target}/`, 302);
     }
   } else if (currentLocale) {
-    cookies.set('locale', currentLocale, { path: '/' });
+    cookies.set("locale", currentLocale, { path: "/" });
   }
 
   return next();
