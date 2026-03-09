631233c8fb56266dec8230a748784fa706780f46
Luuk <mail@luuk.network>
2026-03-09 21:22:33 +0100

    Honor Astro trailingSlash config in cleanPath

--- src/utils/clean-path.ts
+++ src/utils/clean-path.ts
@@ -1,5 +1,11 @@
+import { trailingSlash } from "astro:config/client"
 
 export function cleanPath(...segments: string[]): string {
   const cleanedSegments = segments.flatMap((s) => s.split("/").filter(Boolean));
-  return `/${cleanedSegments.join("/")}/`;
+
+  if (trailingSlash === "always") {
+    return `/${cleanedSegments.join("/")}/`;
+  }
+
+  return `/${cleanedSegments.join("/")}`;
 }
