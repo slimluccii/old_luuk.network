41ed1bfe500ce033573cd4e5daf05ad7c6eea401
Luuk <mail@luuk.network>
2026-03-09 21:18:21 +0100

    Update clean-path.ts

--- src/utils/clean-path.ts
+++ src/utils/clean-path.ts
@@ -1,13 +1,5 @@
-import config from "astro.config";
 
 export function cleanPath(...segments: string[]): string {
-  const { trailingSlash } = config;
-
   const cleanedSegments = segments.flatMap((s) => s.split("/").filter(Boolean));
-
-  if (trailingSlash === "always") {
-    return `/${cleanedSegments.join("/")}/`;
-  }
-
-  return `/${cleanedSegments.join("/")}`;
+  return `/${cleanedSegments.join("/")}/`;
 }
